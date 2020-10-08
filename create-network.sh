92fbf4c09a2a462778d00842bc867b5ca931252d
Pipekung <cj.pipekung@gmail.com>
2020-10-08 11:32:46 +0700

    Update create-network.sh

--- create-network.sh
+++ create-network.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker network create -d overlay --scope swarm --attachable reverse-proxy-net
+#docker network create -d overlay --scope swarm --attachable reverse-proxy-net
+docker network create rev-proxy
